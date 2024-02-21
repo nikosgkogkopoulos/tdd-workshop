87c341324895aeda8dbb9e4c7ba71e972d39e3f2
Nikos Gkogkopoulos <nikos.gkogkopoulos@gmail.com>
2024-02-21 16:28:15 +0200

    Add spares functionality

--- app/Http/Controllers/BowlingCalculator.php
+++ app/Http/Controllers/BowlingCalculator.php
@@ -4,14 +4,68 @@ namespace App\Http\Controllers;
 
 class BowlingCalculator extends Controller
 {
-    public function calculateScore(array $frames):int
+
+    private bool $next_throw_is_double = false;
+    public function calculateScore(array $throws):int
     {
         $total_score = 0;
+        $frames = [];
 
-        foreach($frames as $frame){
-            $total_score += $frame;
+        // create frames
+        while(count($throws) >= 2){
+            $frame = [$throws[0], $throws[1]];
+            $frames[] = $frame;
+            $throws = $this->removeAlreadyUsedThrows($throws);
+        }
+
+        // calculate frames
+        foreach($frames as $current_frame){
+            $current_frame_score = $current_frame[0] + $current_frame[1];
+            if($this->next_throw_is_double === true){
+                $total_frame_score = $current_frame_score + $current_frame[0];
+                $this->next_throw_is_double = false;
+            }
+            else{
+                $total_frame_score = $current_frame_score;
+            }
+
+            $this->checkIfSpare($current_frame_score);
+
+            $total_score += $total_frame_score;
+            var_dump($total_score);
+            $frames = $this->removeAlreadyUsedFrames($frames);
+        }
+
+        // calculate throws
+        foreach($throws as $throw){
+            if($this->next_throw_is_double === true){
+                $total_score += ($throw * 2);
+                $this->next_throw_is_double = false;
+            }else{
+                $total_score += $throw;
+            }
         }
 
         return $total_score;
+    }
+
+    private function checkIfSpare( $current_frame_score):void
+    {
+        if($current_frame_score == 10){
+            $this->next_throw_is_double = true;
+        }
+    }
+
+    private function removeAlreadyUsedThrows(array $throws):array
+    {
+        unset($throws[0]);
+        unset($throws[1]);
+        return array_values($throws);
+    }
+
+    private function removeAlreadyUsedFrames(array $frames):array
+    {
+        unset($frames[0]);
+        return array_values($frames);
     }
 }
--- tests/Unit/BowlingTest.php
+++ tests/Unit/BowlingTest.php
@@ -19,10 +19,10 @@ class BowlingTest extends TestCase
         $result = app(BowlingCalculator::class)->calculateScore([]);
 
         // ASSERT
-        $this->assertEquals($result, $expected_result);
+        $this->assertEquals($expected_result, $result);
     }
 
-        /**
+    /**
      * @test
      */
     public function it_returns_one_for_1_pin_hit(): void
@@ -34,6 +34,51 @@ class BowlingTest extends TestCase
         $result = app(BowlingCalculator::class)->calculateScore([1]);
 
         // ASSERT
-        $this->assertEquals($result, $expected_result);
+        $this->assertEquals($expected_result, $result);
+    }
+
+    /**
+     * @test
+     */
+    public function it_returns_twelve_for_1_spare_with_four_and_six_and_1_pin_hit(): void
+    {
+        // ARRANGE
+        $expected_result = 12;
+
+        // ACT
+        $result = app(BowlingCalculator::class)->calculateScore([4,6,1]);
+
+        // ASSERT
+        $this->assertEquals($expected_result, $result);
+    }
+
+    /**
+     * @test
+     */
+    public function it_returns_ten_for_no_spare_with_4_5_and_1_pin_hit(): void
+    {
+        // ARRANGE
+        $expected_result = 10;
+
+        // ACT
+        $result = app(BowlingCalculator::class)->calculateScore([4,5,1,0]);
+
+        // ASSERT
+        $this->assertEquals($expected_result, $result);
+    }
+
+    /**
+     * @test
+     */
+    public function it_returns_33_after_back_to_back_spares(): void
+    {
+        // ARRANGE
+        $expected_result = 33;
+
+        // ACT
+        $result = app(BowlingCalculator::class)->calculateScore([4,6,1,9,5,2]);
+
+        // ASSERT
+        $this->assertEquals($expected_result, $result);
     }
 }
