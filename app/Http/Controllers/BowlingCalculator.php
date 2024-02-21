d5a2c5400ad4d6457d23a0fc22f189039e932d4e
Nikos Gkogkopoulos <nikos.gkogkopoulos@gmail.com>
2024-02-21 14:16:26 +0200

    Add second test

--- app/Http/Controllers/BowlingCalculator.php
+++ app/Http/Controllers/BowlingCalculator.php
@@ -2,13 +2,16 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 class BowlingCalculator extends Controller
 {
     public function calculateScore(array $frames):int
     {
         $total_score = 0;
+
+        foreach($frames as $frame){
+            $total_score += $frame;
+        }
+
         return $total_score;
     }
 }
--- tests/Unit/BowlingTest.php
+++ tests/Unit/BowlingTest.php
@@ -9,15 +9,29 @@ class BowlingTest extends TestCase
 {
     /**
      * @test
-     * @return void
      */
-    public function it_returns_zero_for_0_throws()
+    public function it_returns_zero_for_0_throws(): void
     {
         // ARRANGE
         $expected_result = 0;
 
         // ACT
         $result = app(BowlingCalculator::class)->calculateScore([]);
+
+        // ASSERT
+        $this->assertEquals($result, $expected_result);
+    }
+
+        /**
+     * @test
+     */
+    public function it_returns_one_for_1_pin_hit(): void
+    {
+        // ARRANGE
+        $expected_result = 1;
+
+        // ACT
+        $result = app(BowlingCalculator::class)->calculateScore([1]);
 
         // ASSERT
         $this->assertEquals($result, $expected_result);
